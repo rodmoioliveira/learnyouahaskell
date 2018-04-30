e55798cb611db72e4d39feba55c662f159cf54ee
rodmoioliveira <rodolfo.oliveira@globoesporte.com>
2018-04-30 03:49:50 -0300

    add chapter 06

---
+++ challenge_openOrSenior.hs
@@ -0,0 +1,55 @@
+-- https://www.codewars.com/kata/categorize-new-member/train/haskell
+
+data Membership = Open | Senior deriving (Eq, Show)
+
+openOrSenior :: [(Int, Int)] -> [Membership]
+openOrSenior [] = []
+openOrSenior xs = [(result x y) | (x,y) <- xs, let result a b = if (a >= 55) && (b > 7) then Senior else Open]
+
+-- Other Solutions
+
+-- =============================================================
+
+-- openOrSenior :: [(Int, Int)] -> [Membership]
+-- openOrSenior = map (\(a,b) -> if a >= 55 && b > 7 then Senior else Open)
+
+-- =============================================================
+
+-- openOrSenior :: [(Int, Int)] -> [Membership]
+-- openOrSenior = map toMembership
+
+-- toMembership :: (Int, Int) -> Membership
+-- toMembership (age, handicap)
+--   | age >= 55 && handicap > 7 = Senior
+--   | otherwise = Open
+
+-- =============================================================
+
+-- openOrSenior :: [(Int, Int)] -> [Membership]
+-- openOrSenior = map openOrSenior'
+--   where openOrSenior' (age,handicap) = if age>=55 && handicap>7 then Senior else Open
+
+-- =============================================================
+
+-- testOpen :: (Int, Int) -> Membership
+-- testOpen (a, h) = if a >= 55 && h > 7 then Senior else Open
+
+-- openOrSenior :: [(Int, Int)] -> [Membership]
+-- openOrSenior [] = []
+-- openOrSenior xs = map testOpen xs
+
+-- =============================================================
+
+-- openOrSenior = map categorize
+--   where categorize (age, handicap)
+--           | 55 <= age && 7 < handicap = Senior
+--           | otherwise                 = Open
+
+-- =============================================================
+
+-- openOrSenior xs = [ let (age, handicap) = x in
+--   if (age >= 55 && handicap > 7) then Senior else Open | x <- xs]
+
+-- =============================================================
+
+-- openOrSenior ((x,y):xs) = (if x >= 55 && y > 7 then Senior else Open) : openOrSenior xs
---
+++ challenge_removeTheMinimum.hs
@@ -0,0 +1,28 @@
+-- https://www.codewars.com/kata/remove-the-minimum/train/haskell
+
+import Data.List
+
+removeSmallest :: Ord t => [t] -> [t]
+removeSmallest [] = []
+removeSmallest xs = xs \\ (minimum xs : [])
+--
+-- Other Solutions
+
+-- =============================================================
+
+-- import Data.List (delete)
+
+-- removeSmallest :: Ord a => [a] -> [a]
+-- removeSmallest xs = delete (minimum xs) xs
+
+-- =============================================================
+
+-- import Data.List (delete)
+
+-- removeSmallest :: [Int] -> [Int]
+-- removeSmallest = delete =<< minimum        MONAD!!!!!!!!!!!!!
+
+-- =============================================================
+
+-- removeSmallest :: [Int] -> [Int]
+-- removeSmallest xs = xs \\ [minimum xs]
